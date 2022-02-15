a7e2e4803210fd47a0ca1f33319df4c43ec28f7d
Saif <saifbenali88@gmail.com>
2022-02-15 12:43:57 +0100

    Final fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './Home.js'
